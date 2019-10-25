f7c31ed06806f0837360906e5a56dd23b92e88e9
J-Anne Carlson <carlsonjc3@J-Annes-MBP.hsd1.wa.comcast.net>
2019-10-24 20:54:21 -0700

    initial commit

---
+++ arrays.js
@@ -0,0 +1,40 @@
+
+// Notes: Another word for argument = imput.
+       //An Array stores a list of values. 
+
+// Example: var colors;
+         //colors = ['white',
+                   //'black',
+                   //'custom'];
+// Access colors: var itemTHREE;
+        // itemTHREE = colors[2];
+
+
+
+// This is an array within an array.
+// To call the values from this array, the place holder is [2];
+
+
+args = [ [ 1, 2 ] ]
+[Array(2)]
+Notes: This is an array within an array.
+To call the values from this array, the place holder is [2].
+
+args[0]; 
+(2) [1, 2]
+args[0][0];
+1 
+args [0][0][0];
+undefined
+args [0][1];
+2
+args = [1, 2, 3, 4];
+(4) [1, 2, 3, 4]
+args [2];
+3
+var arr = [1, [10, 20], 6]
+undefined
+
+arr [1][1];
+20
+var arr = [1, [10, [20], 6];
